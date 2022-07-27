52da42f5d6970e820001ea225b2f6cd9ef9ea853
Joshua Steed <joshuasteed123@gmail.com>
2022-07-27 16:08:10 +0200

    categories route

---
+++ routes/catergoryRoute.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const con = require("../lib/db_connection");
+router.get("/categories", (req, res) => {
+  try {
+    con.query("SELECT * FROM categories", (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+router.post("/", (req, re) => {
+  const {
+      name,
+      description,
+      thumbnail
+  } = req.body;
+  try {
+    con.query(
+      "INSERT INTO categories ('${name}', '${description}', '${thumbnail}",
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+module.exports = router;

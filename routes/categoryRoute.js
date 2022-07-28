ceae54c9535087230e302b0f5983a4f1978dd25d
Joshua Steed <joshuasteed123@gmail.com>
2022-07-28 09:33:23 +0200

    massive changes

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -12,11 +12,11 @@ router.get("/", (req, res) => {
     res.status(400).send(error);
   }
 });
-router.post("/", (req, re) => {
+router.post("/", (req, res) => {
   const { name, description, thumbnail } = req.body;
   try {
     con.query(
-      "INSERT INTO categories ('${name}', '${description}', '${thumbnail}",
+      `INSERT INTO categories (name, description, thumbnail) values ('${name}', '${description}', '${thumbnail}', '${billing_address}', '${default_shipping_address}', '${country}', '${phone}', '${user_type}')`,
       (err, result) => {
         if (err) throw err;
         res.send(result);
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const con = require("../lib/db_connection");
+
 router.get("/", (req, res) => {
   try {
     con.query("SELECT * FROM users", (err, result) => {
@@ -12,7 +13,8 @@ router.get("/", (req, res) => {
     res.status(400).send(error);
   }
 });
-router.post("/", (req, re) => {
+
+router.post("/", (req, res) => {
   const {
     email,
     password,
@@ -25,14 +27,15 @@ router.post("/", (req, re) => {
   } = req.body;
   try {
     con.query(
-      "INSERT INTO users(email, password, full_name,billing_address, default_shipping_address, country, phone, user_type) VALUES ('${email}', '${password}', '${full_name}', '${billing_address}', '${default_shipping_address}', '${country}', '${phone}', '${user_type}'",
+      `INSERT INTO users (email, password, full_name, billing_address, default_shipping_address, country, phone, user_type) values ('${email}', '${password}', '${full_name}', '${billing_address}', '${default_shipping_address}', '${country}', '${phone}', '${user_type}')`,
       (err, result) => {
         if (err) throw err;
         res.send(result);
       }
     );
   } catch (error) {
-    console.log(error);
+    console.log(err);
   }
 });
+
 module.exports = router;
